fd2a4b401c4d54c3e51670b00360d4e60553e339
JuliaMod <julmodlinski@gmail.com>
2021-02-22 13:28:29 +0100

    - added map display

---
+++ lib/utils/ui/screens/parking_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ParkingScreen extends StatefulWidget {
+  @override
+  _ParkingScreenState createState() => _ParkingScreenState();
+}
+
+class _ParkingScreenState extends State<ParkingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
