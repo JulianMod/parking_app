da892d46462f52eae7781b9c9719e8ce9dfa4210
JuliaMod <julmodlinski@gmail.com>
2021-02-22 14:45:01 +0100

    - map is updated to the current location - added pedantic lints

--- lib/utils/ui/screens/map_screen.dart
+++ lib/utils/ui/screens/map_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:parking_app/utils/services/location.dart';
+
 
 class MapScreen extends StatefulWidget {
   @override
@@ -8,21 +10,49 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
 
+  //this could be last location saved
+  final LatLng _initialCameraPosition = LatLng(45.521563, -122.677433);
+
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  Future<void> getLocation() async {
+    Location location = Location();
+    //the location is requested and after it is received the view on the map is automatically updated
+    await location.getCurrentLocation().then((value) {
+      CameraPosition _currentLocation = CameraPosition(
+          target: LatLng(location.latitude, location.longitude),
+          zoom: 15.0);
+      mapController.animateCamera(CameraUpdate.newCameraPosition(_currentLocation));
+    });
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    getLocation();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 11.0,
+    return Scaffold(
+      body: GoogleMap(
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _initialCameraPosition,
+          zoom: 11.0,
+        ),
       ),
     );
   }
