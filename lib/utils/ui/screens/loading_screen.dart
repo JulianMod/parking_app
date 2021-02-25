5c9787f699c8d401858f4d0caddacffdf3945dd2
JuliaMod <julmodlinski@gmail.com>
2021-02-25 16:34:00 +0100

    - added comments to loading_screen.dart - added permission and service check to Location class

--- lib/utils/ui/screens/loading_screen.dart
+++ lib/utils/ui/screens/loading_screen.dart
@@ -5,6 +5,7 @@ import 'package:parking_app/utils/services/location.dart';
 
 LatLng currentLocation;
 
+///loading screen to receive current location and Firebase data before the map is loaded
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -15,6 +16,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   double latitude;
   double longitude;
 
+  //get the location and data from Firebase
   @override
   void initState() {
     FirebaseProvider().getParkingSpots();
@@ -22,11 +24,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
     super.initState();
   }
 
+  ///method to receive current user location and navigate to map screen
   Future<void> getLocation() async {
     Location location = Location();
     await location.getCurrentLocation().then((value) {
-     currentLocation = LatLng(location.latitude, location.longitude);
+      currentLocation = LatLng(location.latitude, location.longitude);
       Navigator.pushNamed(context, '/map_screen');
+      Navigator.pushReplacementNamed(context, '/map_screen');
     });
   }
 
@@ -34,7 +38,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: CircularProgressIndicator()
+          child: CircularProgressIndicator()
       ),
     );
   }
